950ee3730d9007bba9bd0b8f76df22e55a2c314f
James Rogers <jarogers095@gmail.com>
2019-12-07 03:20:28 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -11,11 +11,11 @@ def display_card_total(card_total)
 end
 
 def prompt_user
-  putes "Type 'h' to hit or 's' to stay"
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
 def end_game
